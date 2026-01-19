7ad609e3668ca91eb888a96aae5cf7620f173b18
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2026-01-19 18:20:47 +0300

    environment checks

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production" || process.env.NODE_ENV == "docker") {
+  require("dotenv").config();
+}
 const express = require("express");
 const expresslayouts = require("express-ejs-layouts");
 const methodOverride = require("method-override");
 const connectDB = require("./server/config/db");
-const session = require('express-session')
-const passport = require('passport')
-const MongoStore = require('connect-mongo')
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
 
+const required = ["GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET", "MONGODB_URI"];
+
+for (const key of required) {
+  if (!process.env[key]) {
+    throw new Error(`Missing required env var: ${key}`);
+  }
+}
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,20 +23,22 @@ const port = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
-  cookie: { maxAge: 86400000} // 24 hours
-}))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+    cookie: { maxAge: 86400000 }, // 24 hours
+  }),
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Connect to database
 connectDB();
-// Static files 
+// Static files
 app.use(express.static("public"));
 // Set template engine
 app.use(expresslayouts);
@@ -39,14 +50,13 @@ app.use("/", require("./server/routes/auth"));
 app.use("/", require("./server/routes/index"));
 app.use("/", require("./server/routes/dashboard"));
 
-
 // Handle 404
-app.get("*", function(req, res)
-  {
-    res.status(404).render("404");
-  })
-
+app.get("*", function (req, res) {
+  res.status(404).render("404");
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  if (process.env.NODE_ENV !== "production") {
+    console.log(`Server is running on port http://localhost:${port}`);
+  }
 });
