2ade4e8dff2c7616841c267c12c07c2ca3fdff80
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2026-01-19 22:40:22 +0300

    New Database Name instead of Test DB

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery' , false);
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (err) {
-        console.log(err);
-    }
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI + "/notes_app");
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-    module.exports = connectDB;
+module.exports = connectDB;
